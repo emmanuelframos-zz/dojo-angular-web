befb83b4e292b83866fcd78ef5b2bf0229366e66
emmanuelf <emmanuelf@ciandt.com>
2016-12-13 14:30:48 -0200

    Dojo Dia 4 - Services

--- app/controllers/people-controller.js
+++ app/controllers/people-controller.js
@@ -1,15 +1,22 @@
-angular.module('people').controller('peopleController', ['MonthsFactory', function (MonthsFactory) {
+angular.module('people').controller('PeopleController', ['MonthsFactory', 'PeopleService', function (MonthsFactory, PeopleService) {
     
-    vm = this;
-
-    vm.people = [{ login: 'EMMANUELF', name: 'Emmanuel Ramos', age: 31, birthDate: '1985-11-29', salary: 8500.99, months: [{'desc': 'aug', 'value': 33}, {'desc': 'sep', 'value': 33}, {'desc': 'oct', 'value': 33}] },
-        { login: 'MVIDOLIN', name: 'Marcos Vidolin', age: 28, birthDate: '1988-05-03', salary: 10600.45, months: [{'desc': 'aug', 'value': 33}, {'desc': 'sep', 'value': 33}, {'desc': 'oct', 'value': 33}] },
-        { login: 'ALAN', name: 'Alan Thiago', age: 31, birthDate: '1985-05-12', salary: 9400.31, months: [{'desc': 'aug', 'value': 33}, {'desc': 'sep', 'value': 33}, {'desc': 'oct', 'value': 33}] },
-        { login: 'LUIZSJ', name: 'Luiz Souza', age: 26, birthDate: '1992-09-01', salary: 9250.23, months: [{'desc': 'aug', 'value': 33}, {'desc': 'sep', 'value': 33}, {'desc': 'oct', 'value': 33}] }
-        ];
+    vm = this;  
     
     vm.getMonths = function(){
         return MonthsFactory.months;
     }   
 
+    vm.getPeople = function(){
+        PeopleService.getPeople().then(function(success) {
+            vm.people = success.data;
+        }, function(error) {
+            console.error(error);
+        })
+    }
+
+    function init(){
+        vm.getPeople();
+    }
+
+    init();
 }]);
